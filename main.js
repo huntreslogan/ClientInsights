195d3bea069da17473c40dde9264b7dbfb83e594
Jessica Hunt <jhunt@twilio.com>
2017-03-28 12:34:55 -0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,18 @@
+$(function() {
+
+
+  $("#button1").click(function(){
+    $("#small").animate({
+      backgroundColor: "#13CA6E"
+    }, 100);
+    $("#medium").animate({
+      backgroundColor: "#13CA6E"
+    }, 200);
+    $("#large").animate({
+      backgroundColor: "#13CA6E"
+    }, 300);
+    $("#evenbigger").animate({
+      backgroundColor: "#13CA6E"
+    }, 400);
+  });
+});
